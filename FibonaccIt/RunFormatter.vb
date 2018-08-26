2bf3be37cc039ded6c05a3a0499995de19459d5c
Christian Berger <c.berger18@gmail.com>
2018-08-26 19:05:33 +0200

    Removed unused code

--- FibonaccIt/RunFormatter.vb
+++ FibonaccIt/RunFormatter.vb
@@ -66,64 +66,52 @@ Public NotInheritable Class RunFormatter
         Instance = New RunFormatter(package)
     End Sub
 
-    ''' <summary>
-    ''' This function is the callback used to execute the command when the menu item is clicked.
-    ''' See the constructor to see how the menu item is associated with this function using
-    ''' OleMenuCommandService service and MenuCommand class.
-    ''' </summary>
-    ''' <param name="sender">Event sender.</param>
-    ''' <param name="e">Event args.</param>
-    ''' 
+	''' <summary>
+	''' This function is the callback used to execute the command when the menu item is clicked.
+	''' See the constructor to see how the menu item is associated with this function using
+	''' OleMenuCommandService service and MenuCommand class.
+	''' </summary>
+	''' <param name="sender">Event sender.</param>
+	''' <param name="e">Event args.</param>
+	''' 
 
-    'Declaration
+	'Declaration
 
-    Private Sub MenuItemCallback(sender As Object, e As EventArgs)
+	Private Sub MenuItemCallback(sender As Object, e As EventArgs)
+		ThreadHelper.ThrowIfNotOnUIThread()
 
-        ThreadHelper.ThrowIfNotOnUIThread()
+		Dim dte As EnvDTE.DTE = TryCast(Package.GetGlobalService(GetType(EnvDTE.DTE)), EnvDTE.DTE)
 
-        'Fibonacci-Magic goes here
-        Dim dte As EnvDTE.DTE = TryCast(Package.GetGlobalService(GetType(EnvDTE.DTE)), EnvDTE.DTE)
+		If dte.ActiveDocument IsNot Nothing Then
+			'turning smart indentation OFF
+			TurnOffFormatting(dte)
+			Dim activeDoc As EnvDTE.TextDocument = TryCast(dte.ActiveDocument.Object(), EnvDTE.TextDocument)
 
-        If dte.ActiveDocument IsNot Nothing Then
+			activeDoc.Selection.SelectAll()
+			activeDoc.Selection.SmartFormat()
+			Dim indentsize As Integer = activeDoc.IndentSize
+			Dim indentdepth As Integer = 0
+			Dim replaceText As String = ""
 
-            'turning smart indentation OFF
-            TurnOffFormatting(dte)
+			For Each line In activeDoc.Selection.Text.Split(Environment.NewLine)
+				'Here i come, /r/ProgrammerHumor - garbage code to remove the vbLf (probably) from the split above
+				line = line.Replace(Chr(10), "")
 
+				indentdepth = (line.Length - line.TrimStart.Length) / indentsize
+				Dim spacenumber = 0
 
-            Dim activeDoc As EnvDTE.TextDocument = TryCast(dte.ActiveDocument.Object(), EnvDTE.TextDocument)
+				For i As Integer = indentdepth To 0 Step -1
+					spacenumber += FibonacciIt(i)
+				Next
+				replaceText = replaceText & Space(spacenumber) & line.TrimStart & vbNewLine
+			Next
+			activeDoc.Selection.Delete()
+			activeDoc.Selection.Insert(replaceText)
 
+		End If
+	End Sub
 
-            activeDoc.Selection.SelectAll()
-            activeDoc.Selection.SmartFormat()
-            Dim indentsize As Integer = activeDoc.IndentSize
-            Dim indentdepth As Integer = 0
-
-            Dim replaceText As String = ""
-
-            'backup code from window before changing anything
-            Dim selText As String = activeDoc.Selection.Text
-
-            For Each line In activeDoc.Selection.Text.Split(Environment.NewLine)
-                'Here i come, /r/ProgrammerHumor - garbage code to remove the vbLf (probably) from the split above
-                line = line.Replace(Chr(10), "")
-
-                indentdepth = (line.Length - line.TrimStart.Length) / indentsize
-
-                Dim spacenumber = 0
-                For i As Integer = indentdepth To 0 Step -1
-                    spacenumber += FibonacciIt(i)
-                Next
-                replaceText = replaceText & Space(spacenumber) & line.TrimStart & vbNewLine
-            Next
-            activeDoc.Selection.Delete()
-            activeDoc.Selection.Insert(replaceText)
-
-        End If
-
-
-    End Sub
-
-    Private Function FibonacciIt(ByVal number As Integer) As Integer
+	Private Function FibonacciIt(ByVal number As Integer) As Integer
 		If number > 2 Then
 			number = FibonacciIt(number - 1) + (FibonacciIt(number - 2))
 		ElseIf number > 0 Then
@@ -131,18 +119,16 @@ Public NotInheritable Class RunFormatter
 		Else
 			number = 0
 		End If
-
-        Return number
+		Return number
     End Function
 
-    Private Sub TurnOffFormatting(ByVal dte As EnvDTE.DTE)
+	Private Sub TurnOffFormatting(ByVal dte As EnvDTE.DTE)
+		Dim p1 As EnvDTE.Properties = dte.Properties("TextEditor", "Basic-Specific")
+		p1.Item("PrettyListing").Value = False
 
-        Dim p1 As EnvDTE.Properties = dte.Properties("TextEditor", "Basic-Specific")
-        p1.Item("PrettyListing").Value = False
-
-        Dim p2 As EnvDTE.Properties = dte.Properties("TextEditor", "AllLanguages")
-        p2.Item("InsertTabs").Value = False
-    End Sub
+		Dim p2 As EnvDTE.Properties = dte.Properties("TextEditor", "AllLanguages")
+		p2.Item("InsertTabs").Value = False
+	End Sub
 
 End Class
 
